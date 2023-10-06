c9420b22093ee81f3848046dada001d362fc06e0
oleno <es.moname@gmail.com>
2023-10-06 18:30:37 +0200

    Add solution for prep exercise week1 API

--- Week1/prep-exercises/1-catwalk-promises/index.js
+++ Week1/prep-exercises/1-catwalk-promises/index.js
@@ -5,22 +5,38 @@ const STEP_INTERVAL_MS = 50;
 const DANCE_TIME_MS = 5000;
 const DANCING_CAT_URL =
   'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+let isDancing = false;
+let hasDanced = false;
 
 function walk(img, startPos, stopPos) {
   return new Promise((resolve) => {
-    // Resolve this promise when the cat (`img`) has walked from `startPos` to
-    // `stopPos`.
-    // Make good use of the `STEP_INTERVAL_PX` and `STEP_INTERVAL_MS`
-    // constants.
+    img.style.left = `${startPos}px`;
+    const interval = setInterval(() => {
+      const currentPos = parseInt(img.style.left);
+
+      if(isDancing) return;
+
+      img.style.left = currentPos + STEP_SIZE_PX + 'px';
+      if(currentPos >= stopPos && !hasDanced){
+        isDancing = true;
+        resolve();
+      } else if ((currentPos >= stopPos) && hasDanced) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, STEP_INTERVAL_MS);
   });
 }
 
 function dance(img) {
   return new Promise((resolve) => {
-    // Switch the `.src` of the `img` from the walking cat to the dancing cat
-    // and, after a timeout, reset the `img` back to the walking cat. Then
-    // resolve the promise.
-    // Make good use of the `DANCING_CAT_URL` and `DANCE_TIME_MS` constants.
+        img.src = DANCING_CAT_URL;
+        setTimeout(() => {
+          isDancing = false;
+          hasDanced = true;
+          img.src = "http://www.anniemation.com/clip_art/images/cat-walk.gif";
+          resolve();
+        }, DANCE_TIME_MS); 
   });
 }
 
@@ -30,11 +46,14 @@ function catWalk() {
   const centerPos = (window.innerWidth - img.width) / 2;
   const stopPos = window.innerWidth;
 
-  // Use the `walk()` and `dance()` functions to let the cat do the following:
-  // 1. Walk from `startPos` to `centerPos`.
-  // 2. Then dance for 5 secs.
-  // 3. Then walk from `centerPos` to `stopPos`.
-  // 4. Repeat the first three steps indefinitely.
+  function repeatCatActions() {
+    walk(img, startPos, centerPos)
+    .then(() => dance(img))
+    .then(() => walk(img, centerPos, stopPos))
+    .then(repeatCatActions);
+  }
+
+  repeatCatActions(); 
 }
 
 window.addEventListener('load', catWalk);
